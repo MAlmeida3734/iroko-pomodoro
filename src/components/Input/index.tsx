49a798fe91c5cc8726488abd5fbe9384904dc9de
Marco Almeida <macunhadealmeida@gmail.com>
2026-02-04 12:34:11 -0300

    Commit inicial

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,17 @@
+import  styles from './styles.module.css'
+
+
 type InputProps = {
-  id: string;
-  labeltext: string;
-} & React.ComponentProps<'input'>;
+  id: string
+  labeltext: string
+} & React.ComponentProps<'input'>
 
-
-export function Input({ id, type, labeltext}: InputProps) {
+export function Input({ id, type, labeltext, ...rest }: InputProps) {
   return (
     <>
       <label htmlFor={id}>{labeltext}</label>
-      <input id={id} type={type} />
+
+      <input className={styles.input} id={id} type={type} {...rest} />
     </>
   )
 }
