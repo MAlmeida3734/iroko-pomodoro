51933600519231561c5863ffcb5f30973e94a1b4
Marco Almeida <macunhadealmeida@gmail.com>
2026-02-11 00:43:11 -0300

    Commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { useState } from 'react'
+import { TaskContextProvider } from './contests/TaskContext'
 import { Home } from './pages/Home'
 import './styles/global.css'
 import './styles/theme.css'
-import type { TaskStateModel } from './models/TaskStateModel';
-
-
-const initialState: TaskStateModel = {
-tasks:[],
-secondsRemaining:0,
-formatedSecondsReimining: '00:00',
-activeTask: null,
-currentCycle: 0,
-config: {
-  workTime: 25,
-  shortBreakTime: 5,
-  longBreakTime: 15
-},
-}; 
-
 
 export function App() {
-const [state, setState]  =  useState(initialState);
-
-console.log( 'APP', state );
-  return <Home />
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  )
 }
